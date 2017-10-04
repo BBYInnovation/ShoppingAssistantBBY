fde3a97546072931b088928a94b9df77f418e004
kumari, Jyoti-CW <joearun.1990@gmail.com>
2017-10-04 17:16:00 +0530

    testing carousel 2

--- index.js
+++ index.js
@@ -51,14 +51,35 @@ function buyPrinter (appAi) {
 }
 
 function choosePrinterType (appAi) {
-  appAi.ask(appAi.buildRichResponse().addBasicCard(appAi.buildBasicCard(`42 is an even composite number. It
-      is composed of three distinct prime numbers multiplied together. It
-      has a total of eight divisors. 42 is an abundant number, because the
-      sum of its proper divisors 54 is greater than itself. To count from
-      1 to 42 would take you about twenty-one…`)
+  console.log("Inside choosePrinterType")
+  appAi.askWithList(appAi.buildRichResponse()
+    .addSimpleResponse('Alright')
+    .addSuggestions(
+      ['Basic Card', 'List', 'Carousel', 'Suggestions']),
+    // Build a list
+    appAi.buildList('Things to learn about')
+    // Add the first item to the list
+    .addItems(appAi.buildOptionItem('MATH_AND_PRIME',
+      ['math', 'math and prime', 'prime numbers', 'prime'])
       .setTitle('Math & prime numbers')
-      .addButton('Read more', 'https://goshopping-130590.herokuapp.com/assets/rift.png')
-      .setImage('https://goshopping-130590.herokuapp.com/assets/rift.png', 'Image alternate text')
+      .setDescription('42 is an abundant number because the sum of its ' +
+        'proper divisors 54 is greater…')
+      .setImage('http://example.com/math_and_prime.jpg', 'Math & prime numbers'))
+    // Add the second item to the list
+    .addItems(appAi.buildOptionItem('EGYPT',
+      ['religion', 'egpyt', 'ancient egyptian'])
+      .setTitle('Ancient Egyptian religion')
+      .setDescription('42 gods who ruled on the fate of the dead in the ' +
+        'afterworld. Throughout the under…')
+      .setImage('http://example.com/egypt', 'Egypt')
+    )
+    // Add third item to the list
+    .addItems(appAi.buildOptionItem('RECIPES',
+      ['recipes', 'recipe', '42 recipes'])
+      .setTitle('42 recipes with 42 ingredients')
+      .setDescription('Here\'s a beautifully simple recipe that\'s full ' +
+        'of flavor! All you need is some ginger and…')
+      .setImage('http://example.com/recipe', 'Recipe')
     )
   );
 }
